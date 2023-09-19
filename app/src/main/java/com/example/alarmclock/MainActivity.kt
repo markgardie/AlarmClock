d77c0d9252ee10a1b295e77129ee48390419a88c
Mark <mark130599@gmail.com>
2023-09-19 19:35:41 +0300

    change structure of navigation

--- app/src/main/java/com/example/alarmclock/MainActivity.kt
+++ app/src/main/java/com/example/alarmclock/MainActivity.kt
@@ -10,7 +10,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.alarmclock.ui.screens.AlarmListScreen
+import com.example.alarmclock.ui.screens.alarmListRoute
+import com.example.alarmclock.ui.screens.alarmListScreen
 import com.example.alarmclock.ui.theme.AlarmClockTheme
 import com.example.alarmclock.ui.viewmodels.AlarmViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,21 +29,24 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AlarmClockTheme {
+
+                val navController = rememberNavController()
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val viewModel by viewModels<AlarmViewModel>()
-                    val uiState by viewModel.uiState.collectAsState()
 
-                    AlarmListScreen(
-                        alarmUiState = uiState,
-                        onAddAlarm = viewModel::addAlarm,
-                        onDeleteAlarm = viewModel::deleteAlarm,
-                        onUpdateAlarm = viewModel::updateAlarm
-                    )
+                    NavHost(
+                        navController = navController,
+                        startDestination = alarmListRoute
+                    ) {
+                        alarmListScreen()
+                    }
+
                 }
+
+
             }
         }
     }
--- app/src/main/java/com/example/alarmclock/ui/screens/AlarmListScreen.kt
+++ app/src/main/java/com/example/alarmclock/ui/screens/AlarmListScreen.kt
@@ -10,53 +10,137 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
 import com.example.alarmclock.domain.AlarmItem
 import com.example.alarmclock.ui.viewmodels.AlarmUiState
+import com.example.alarmclock.ui.viewmodels.AlarmViewModel
+
+const val alarmListRoute = "alarm_list"
+
+fun NavController.navigateToAlarmList(navOptions: NavOptions? = null) {
+    this.navigate(alarmListRoute, navOptions)
+}
+
+fun NavGraphBuilder.alarmListScreen() {
+    composable(
+        route = alarmListRoute
+    ) {
+
+        AlarmListRoute()
+    }
+}
+
+@Composable
+fun AlarmListRoute(
+    viewModel: AlarmViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    AlarmListScreen(
+        alarmUiState = uiState,
+        onAddAlarm = viewModel::addAlarm,
+        onDeleteAlarm = viewModel::deleteAlarm,
+        onUpdateAlarm = viewModel::updateAlarm,
+        modifier = modifier
+    )
+
+}
 
 @Composable
 fun AlarmListScreen(
     alarmUiState: AlarmUiState,
     onAddAlarm: ((AlarmItem) -> Unit)? = null,
     onDeleteAlarm: ((Int) -> Unit)? = null,
-    onUpdateAlarm: ((AlarmItem) -> Unit)? = null
+    onUpdateAlarm: ((AlarmItem) -> Unit)? = null,
+    modifier: Modifier = Modifier
 ) {
 
     when(alarmUiState) {
-        is AlarmUiState.Success ->
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                items(alarmUiState.alarms) {
+        is AlarmUiState.Success -> AlarmsList(
+            modifier = modifier,
+            alarmUiState.alarms,
+            onDeleteAlarm,
+            onUpdateAlarm
+        )
 
-                }
-            }
-        is AlarmUiState.Empty ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "No Alarms"
-                )
+        is AlarmUiState.Empty -> EmptyState(modifier = modifier)
 
-            }
-        is AlarmUiState.Loading ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                CircularProgressIndicator()
-            }
+        is AlarmUiState.Loading -> LoadingState(modifier = modifier)
+
     }
 
+}
+
+@Composable
+fun AlarmsList(
+    modifier: Modifier = Modifier,
+    alarms: List<AlarmItem>,
+    onDeleteAlarm: ((Int) -> Unit)? = null,
+    onUpdateAlarm: ((AlarmItem) -> Unit)? = null
+) {
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        items(alarms) {
+
+        }
+    }
+}
+
+@Composable
+fun AlarmCard(
+    modifier: Modifier = Modifier,
+    alarm: AlarmItem,
+    onDeleteAlarm: ((Int) -> Unit)? = null,
+    onUpdateAlarm: ((AlarmItem) -> Unit)? = null
+) {
+
+}
+
+@Composable
+fun EmptyState(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "No Alarms"
+        )
+
+    }
+
+}
+
+@Composable
+fun LoadingState(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator()
+    }
 }
