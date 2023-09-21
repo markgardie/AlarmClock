27c16a929b664e0c5e598e656c7f734337628c87
Mark <mark130599@gmail.com>
2023-09-21 19:51:36 +0300

    create alarm card and FAB

--- app/src/main/java/com/example/alarmclock/ui/screens/AlarmListScreen.kt
+++ app/src/main/java/com/example/alarmclock/ui/screens/AlarmListScreen.kt
@@ -1,21 +1,32 @@
 package com.example.alarmclock.ui.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
@@ -43,8 +54,7 @@ fun NavGraphBuilder.alarmListScreen() {
 
 @Composable
 fun AlarmListRoute(
-    viewModel: AlarmViewModel = hiltViewModel(),
-    modifier: Modifier = Modifier
+    viewModel: AlarmViewModel = hiltViewModel(), modifier: Modifier = Modifier
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
@@ -61,13 +71,13 @@ fun AlarmListRoute(
 @Composable
 fun AlarmListScreen(
     alarmUiState: AlarmUiState,
-    onAddAlarm: ((AlarmItem) -> Unit)? = null,
-    onDeleteAlarm: ((Int) -> Unit)? = null,
-    onUpdateAlarm: ((AlarmItem) -> Unit)? = null,
+    onAddAlarm: (AlarmItem) -> Unit,
+    onDeleteAlarm: (Int) -> Unit,
+    onUpdateAlarm: (AlarmItem) -> Unit,
     modifier: Modifier = Modifier
 ) {
 
-    when(alarmUiState) {
+    when (alarmUiState) {
         is AlarmUiState.Success -> AlarmsList(
             modifier = modifier,
             alarmUiState.alarms,
@@ -81,34 +91,73 @@ fun AlarmListScreen(
 
     }
 
+    Box(contentAlignment = Alignment.BottomEnd) {
+        FloatingActionButton(
+            modifier = modifier.padding(16.dp),
+            onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Filled.Add, contentDescription = "Add new alarm")
+        }
+    }
+
 }
 
 @Composable
 fun AlarmsList(
     modifier: Modifier = Modifier,
     alarms: List<AlarmItem>,
-    onDeleteAlarm: ((Int) -> Unit)? = null,
-    onUpdateAlarm: ((AlarmItem) -> Unit)? = null
+    onDeleteAlarm: (Int) -> Unit,
+    onUpdateAlarm: (AlarmItem) -> Unit
 ) {
 
     LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         items(alarms) {
-
+            AlarmCard(
+                alarm = it,
+                onDeleteAlarm = onDeleteAlarm,
+                onUpdateAlarm = onUpdateAlarm
+            )
         }
     }
 }
+
 
 @Composable
 fun AlarmCard(
     modifier: Modifier = Modifier,
     alarm: AlarmItem,
-    onDeleteAlarm: ((Int) -> Unit)? = null,
-    onUpdateAlarm: ((AlarmItem) -> Unit)? = null
+    onDeleteAlarm: (Int) -> Unit,
+    onUpdateAlarm: (AlarmItem) -> Unit
 ) {
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = modifier.padding(8.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = alarm.time, fontSize = 24.sp
+                )
+                Text(
+                    text = alarm.title, fontSize = 14.sp
+                )
+            }
 
+            var checked by remember {
+                mutableStateOf(alarm.enabled)
+            }
+
+            Switch(modifier = modifier.padding(8.dp), checked = checked, onCheckedChange = {
+                checked = it
+                onUpdateAlarm?.invoke(alarm.copy(enabled = it))
+            })
+        }
+    }
 }
 
 @Composable
@@ -117,8 +166,7 @@ fun EmptyState(
 ) {
 
     Column(
-        modifier = modifier
-            .fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
@@ -136,11 +184,11 @@ fun LoadingState(
 ) {
 
     Column(
-        modifier = modifier
-            .fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         CircularProgressIndicator()
     }
 }
+
