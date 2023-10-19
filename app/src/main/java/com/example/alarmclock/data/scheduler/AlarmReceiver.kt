bd37c3258d72ae7edfe50793589a2f1d08d0f0d4
Mark <mark130599@gmail.com>
2023-10-19 14:39:39 +0300

    add notification

--- app/src/main/java/com/example/alarmclock/data/scheduler/AlarmReceiver.kt
+++ app/src/main/java/com/example/alarmclock/data/scheduler/AlarmReceiver.kt
@@ -1,14 +1,37 @@
 package com.example.alarmclock.data.scheduler
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.example.alarmclock.R
 
 class AlarmReceiver: BroadcastReceiver() {
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val alarmTitle = intent?.getStringExtra("EXTRA_TITLE") ?: "null"
+    companion object {
 
-        println("Alarm triggered: $alarmTitle")
+        const val CHANNEL_ID = "alarm_id"
+        const val CHANNEL_NAME = "Alarm Clock"
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val alarmTitle = intent?.getStringExtra("EXTRA_TITLE") ?: return
+
+        context?.let { context ->
+
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val notification = NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Alarm Clock")
+                .setContentText(alarmTitle)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build()
+
+            notificationManager.notify(1, notification)
+        }
     }
 }
--- app/src/main/java/com/example/alarmclock/di/AlarmClockApp.kt
+++ app/src/main/java/com/example/alarmclock/di/AlarmClockApp.kt
@@ -1,8 +1,33 @@
 package com.example.alarmclock.di
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.example.alarmclock.data.scheduler.AlarmReceiver
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class AlarmClockApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                AlarmReceiver.CHANNEL_ID,
+                AlarmReceiver.CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            channel.description = "Basic channel for Alarm Clock"
+
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
 }
